c9a6e57d45811fca5fb0abf9b6652d55b63e84ba
TroyBohrer8 <81994337+TroyBohrer8@users.noreply.github.com>
2021-10-21 13:34:50 -0400

    created event listener that renders search history and saves in local storage

--- script.js
+++ script.js
@@ -110,6 +110,14 @@ function createPage() {
       }
     });
   }
+
+  searchEl.addEventListener('click', function() {
+    const searchTerm = inputEl.value;
+    getWeather(searchTerm);
+    searchHistory.push(searchTerm);
+    localStorage.setItem("search",JSON.stringify(searchHistory));
+    renderSearchHistory();
+  })
 }
 
 createPage();
