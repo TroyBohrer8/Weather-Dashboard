855900ce6518f977edac2102fa718e4f1deb3b5b
TroyBohrer8 <81994337+TroyBohrer8@users.noreply.github.com>
2021-10-21 12:30:33 -0400

    created variables and linked to html id's

--- script.js
+++ script.js
@@ -1,44 +1,22 @@
-// api key = b093c629e348c4ebc81caa5c77847e90
-const APIKey = '4b95c685f1562c4add191dadc0d69d6c';
+function createPage() {
+  const inputEl = document.getElementById("city-input");
+  const searchEl = document.getElementById("search-button");
+  const clearEl = document.getElementById("clear-history");
+  const nameEl = document.getElementById("city-name");
+  const currentPicEl = document.getElementById("current-pic");
+  const currentTempEl = document.getElementById("temperature");
+  const currentHumidityEl = document.getElementById("humidity");4
+  const currentWindEl = document.getElementById("wind-speed");
+  const currentUVEl = document.getElementById("UV-index");
+  const historyEl = document.getElementById("history");
+  let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+  console.log(searchHistory);
 
-const results = document.querySelector('.city-results');
-const cityName = document.getElementById('searchButton');
-
-function getWeather () {
-
-
-}
-function getApi (cityName) {
-  
-  const requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid='+ APIKey
-  
-  fetch(requestUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data)
-      for (var i = 0; i < data.length; i++) {
-        const listItem = document.createElement('li');
-        listItem.textContent = data[i].html_url;
-        results.append(listItem);
-
-      }
-    })
-}
-
-function handleSubmit () {
-
-
-
-}
-
-function fiveDay() {
-
-
+  const APIKey = '4b95c685f1562c4add191dadc0d69d6c';
 }
 
 
 
 
-searchButton.addEventListener('click', getApi);
+
+
