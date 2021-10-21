bd844f2703908bc78a5889f6022fe343340bc664
TroyBohrer8 <81994337+TroyBohrer8@users.noreply.github.com>
2021-10-21 13:42:32 -0400

    re-did css file

--- script.js
+++ script.js
@@ -130,18 +130,23 @@ function createPage() {
 
   function renderSearchHistory() {
     historyEl.innerHTML = "";
-    for (let i=0; i<searchHistory.length; i++) {
-        const historyItem = document.createElement("input");
-        historyItem.setAttribute("type","text");
-        historyItem.setAttribute("readonly",true);
-        historyItem.setAttribute("class", "form-control d-block bg-white");
-        historyItem.setAttribute("value", searchHistory[i]);
-        historyItem.addEventListener("click",function() {
-            getWeather(historyItem.value);
-        })
-        historyEl.append(historyItem);
+    for (let i = 0; i < searchHistory.length; i++) {
+      const historyItem = document.createElement("input");
+      historyItem.setAttribute("type", "text");
+      historyItem.setAttribute("readonly", true);
+      historyItem.setAttribute("class", "form-control d-block bg-white");
+      historyItem.setAttribute("value", searchHistory[i]);
+      historyItem.addEventListener("click", function () {
+        getWeather(historyItem.value);
+      });
+      historyEl.append(historyItem);
     }
-}
+  }
+
+  renderSearchHistory();
+  if (searchHistory.length > 0) {
+    getWeather(searchHistory[searchHistory.length - 1]);
+  }
 }
 
 createPage();
