cfd0becb250f7ec0128beb655bfdc9740c7b9727
TroyBohrer8 <81994337+TroyBohrer8@users.noreply.github.com>
2021-10-21 13:18:56 -0400

    set up current date weather elements

--- script.js
+++ script.js
@@ -5,18 +5,45 @@ function createPage() {
   const nameEl = document.getElementById("city-name");
   const currentPicEl = document.getElementById("current-pic");
   const currentTempEl = document.getElementById("temperature");
-  const currentHumidityEl = document.getElementById("humidity");4
+  const currentHumidityEl = document.getElementById("humidity");
+  4;
   const currentWindEl = document.getElementById("wind-speed");
   const currentUVEl = document.getElementById("UV-index");
   const historyEl = document.getElementById("history");
   let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
   console.log(searchHistory);
 
-  const APIKey = '4b95c685f1562c4add191dadc0d69d6c';
+  // api key
+  const APIKey = "4b95c685f1562c4add191dadc0d69d6c";
+
+  function getWeather(cityName) {
+    let weatherURL =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      cityName +
+      "&appid=" +
+      APIKey;
+    axios.get(weatherURL).then(function (response) {
+      console.log(response);
+      // parse response to display current weather conditions
+      const currentDate = new Date(response.data.dt * 1000);
+      console.log(currentDate);
+      const day = currentDate.getDate();
+      const month = currentDate.getMonth() + 1;
+      const year = currentDate.getFullYear();
+      nameEl.innerHTML =
+        response.data.name + " (" + month + "/" + day + "/" + year + ") ";
+      let weatherPic = response.data.weather[0].icon;
+      currentPicEl.setAttribute(
+        "src",
+        "https://openweathermap.org/img/wn/" + weatherPic + "@2x.png"
+      );
+      currentPicEl.setAttribute("alt", response.data.weather[0].description);
+      currentTempEl.innerHTML =
+        "Temperature: " + k2f(response.data.main.temp) + " &#176F";
+      currentHumidityEl.innerHTML =
+        "Humidity: " + response.data.main.humidity + "%";
+      currentWindEl.innerHTML =
+        "Wind Speed: " + response.data.wind.speed + " MPH";
+    });
+  }
 }
-
-
-
-
-
-
