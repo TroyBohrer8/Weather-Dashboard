0b80a12481667c55f1a54f0a90b9922d61be444d
TroyBohrer8 <81994337+TroyBohrer8@users.noreply.github.com>
2021-10-21 12:27:49 -0400

    re-did index.html page

--- script.js
+++ script.js
@@ -1,18 +1,44 @@
-function getApi () {
-    var requestUrl = ''
+// api key = b093c629e348c4ebc81caa5c77847e90
+const APIKey = '4b95c685f1562c4add191dadc0d69d6c';
 
-        fetch(requestUrl)
-        .then(function (response) {
-        return response.json();
+const results = document.querySelector('.city-results');
+const cityName = document.getElementById('searchButton');
+
+function getWeather () {
+
+
+}
+function getApi (cityName) {
+  
+  const requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid='+ APIKey
+  
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
     })
-        .then(function (data) {
-        console.log(data);
-  // TODO: Loop through the response
-        for (var i = 0; i < requestUrl.length; i + 1)
-    
-  // TODO: Console log each issue's URL and each user's login
-        console.log(requestUrl[i].user)
-});
+    .then(function (data) {
+      console.log(data)
+      for (var i = 0; i < data.length; i++) {
+        const listItem = document.createElement('li');
+        listItem.textContent = data[i].html_url;
+        results.append(listItem);
+
+      }
+    })
+}
+
+function handleSubmit () {
+
+
 
 }
 
+function fiveDay() {
+
+
+}
+
+
+
+
+searchButton.addEventListener('click', getApi);
